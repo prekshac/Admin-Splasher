f19f1e2701d87e9366d5194a25e205cc7804f7fd
Ankitasingh14 <as2793966@gmail.com>
2023-04-24 17:03:22 +0530

    .....

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,13 +4,436 @@ import './Home.css';
 const Home = () => {
   return (
     <div>
-        <h1 style={{ color : 'red', width: 'fit-content' }}>Welcome to Home </h1>
+  <div data-draggable="true" style={{ position: "relative" }}>
+    {/**/}
+    {/**/}
+    <section draggable="false" className="container pt-5" data-v-271253ee="">
+      <section className="mb-10 text-center">
+        <h2 className="fw-bold mb-5 text-center">
+          {" "}
+          <span className="me-1">Why is it so</span>
+          <u className="text-primary">great?</u>{" "}
+        </h2>
+        <div className="row gx-lg-5">
+          <div className="col-md-4 mb-5 mb-md-0">
+            <div className="p-3 bg-primary rounded-4 shadow-2-strong d-inline-block mb-4">
+              {" "}
+              <i
+                className="fas fa-headset fa-lg text-white fa-fw"
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="fw-bold mb-3">Support 24/7</h5>
+            <p className="text-muted mb-0">
+              Laudantium totam quas cumque pariatur at doloremque hic quos quia
+              eius. Reiciendis optio minus mollitia rerum labore facilis
+              inventore voluptatem ad, quae quia sint. Ullam.
+            </p>
+          </div>
+          <div className="col-md-4 mb-5 mb-md-0">
+            <div className="p-3 bg-primary rounded-4 shadow-2-strong d-inline-block mb-4">
+              {" "}
+              <i
+                className="fas fa-shield-alt fa-lg text-white fa-fw"
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="fw-bold mb-3">Safe and solid</h5>
+            <p className="text-muted mb-0">
+              Eum nostrum fugit numquam, voluptates veniam neque quibusdam ullam
+              aspernatur odio soluta, quisquam dolore animi mollitia a omnis
+              praesentium, expedita nobis!
+            </p>
+          </div>
+          <div className="col-md-4 mb-5 mb-md-0">
+            <div className="p-3 bg-primary rounded-4 shadow-2-strong d-inline-block mb-4">
+              {" "}
+              <i
+                className="fas fa-shipping-fast fa-lg text-white fa-fw"
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="fw-bold mb-3">Extremely fast</h5>
+            <p className="text-muted mb-0">
+              Enim cupiditate, minus nulla dolor cumque iure eveniet facere
+              ullam beatae hic voluptatibus dolores exercitationem? Facilis
+              debitis aspernatur amet nisi?
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+    {/**/}
+  </div>
+  <div data-draggable="true" style={{ position: "relative" }}>
+    {/**/}
+    {/**/}
+    <section
+      draggable="false"
+      className="overflow-hidden pt-0"
+      data-v-271253ee=""
+    >
+      <section className="mb-10">
+        {" "}
+        {/* Background image */}
+        <div
+          className="p-5 text-center bg-image"
+          style={{
+            backgroundImage:
+              'url("https://mdbootstrap.com/img/new/textures/full/142.jpg")',
+            height: 500,
+            backgroundSize: "cover",
+            backgroundPosition: "50% 50%",
+            backgroundColor: "rgba(0, 0, 0, 0)"
+          }}
+          aria-controls="#picker-editor"
+        />{" "}
+        {/* Background image */}
+        <div className="container">
+          <div
+            className="card mx-4 mx-md-5 text-center shadow-5-strong"
+            style={{
+              marginTop: "-170px",
+              background: "hsla(0, 0%, 100%, 0.7)",
+              backdropFilter: "blur(30px)"
+            }}
+          >
+            <div className="card-body px-4 py-5 px-md-5">
+              <h1 className="display-3 fw-bold ls-tight mb-4">
+                {" "}
+                <span>The best offer on the market</span> <br />{" "}
+                <span className="text-primary">for your business</span>{" "}
+              </h1>{" "}
+              <a
+                className="btn  btn-primary btn-lg py-3 px-5 mb-2 mb-md-0 me-md-2"
+                href="#"
+                role="button"
+                aria-controls="#picker-editor"
+              >
+                Get started
+              </a>{" "}
+              <a
+                className="btn  btn-link btn-lg py-3 px-5 mb-2 mb-md-0"
+                data-ripple-color="primary"
+                href="#"
+                role="button"
+                aria-controls="#picker-editor"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+    {/**/}
+  </div>
+  <div data-draggable="true" style={{ position: "relative" }}>
+    {/**/}
+    {/**/}
+    <section draggable="false" className="container pt-5" data-v-271253ee="">
+      <section className="mb-10">
+        <style
+          dangerouslySetInnerHTML={{
+            __html:
+              "\n              .rounded-t-5 {\n                border-top-left-radius: 0.5rem;\n                border-top-right-radius: 0.5rem;\n              }\n\n              @media (min-width: 992px) {\n                .rounded-tr-lg-0 {\n                  border-top-right-radius: 0;\n                }\n\n                .rounded-bl-lg-5 {\n                  border-bottom-left-radius: 0.5rem;\n                }\n              }\n            "
+          }}
+        />
+        <div className="card mb-3">
+          <div className="row g-0 d-flex align-items-center">
+            <div className="col-lg-4 d-none d-lg-flex">
+              {" "}
+              <img
+                src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
+                alt="Trendy Pants and Shoes"
+                className="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5"
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <div className="col-lg-8">
+              <div className="card-body py-5 px-md-5">
+                <h2 className="fw-bold mb-4">One-time payment</h2>
+                <p className="text-muted mb-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                  soluta corporis voluptate ab error quam dolores doloremque,
+                  quae consectetur.
+                </p>
+                <div className="row mb-3">
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Support 24/7</span>{" "}
+                    </p>
+                  </div>
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Analytics</span>{" "}
+                    </p>
+                  </div>
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Components</span>{" "}
+                    </p>
+                  </div>
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Updates</span>{" "}
+                    </p>
+                  </div>
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Reports</span>{" "}
+                    </p>
+                  </div>
+                  <div className="col-lg-4 mb-2">
+                    <p>
+                      {" "}
+                      <i className="fas fa-check-circle text-success me-2" />
+                      <span>Mobile</span>{" "}
+                    </p>
+                  </div>
+                </div>{" "}
+                <button type="button" className="btn btn-primary btn-lg px-5">
+                  Buy now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+    {/**/}
+  </div>
+  <div data-draggable="true" style={{ position: "relative" }}>
+    {/**/}
+    {/**/}
+    <section draggable="false" className="container pt-5" data-v-271253ee="">
+      <section className="mb-10 text-center">
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-10 col-xl-8">
+            <h2 className="fw-bold mb-5">Testimonials</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4 mb-5 mb-md-0">
+            <div className="d-flex justify-content-center mb-4">
+              {" "}
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg"
+                className="rounded-circle shadow-1-strong"
+                width={150}
+                height={150}
+                alt=""
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="mb-3">Maria Smantha</h5>
+            <h6 className="text-primary mb-3">Web Developer</h6>
+            <p className="px-xl-3">
+              {" "}
+              <i className="fas fa-quote-left pe-2" />
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                eos id officiis hic tenetur quae quaerat ad velit ab hic
+                tenetur.
+              </span>{" "}
+            </p>
+            <ul className="list-unstyled d-flex justify-content-center mb-0">
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star-half-alt fa-sm text-warning" />{" "}
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 mb-5 mb-md-0">
+            <div className="d-flex justify-content-center mb-4">
+              {" "}
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(2).jpg"
+                className="rounded-circle shadow-1-strong"
+                width={150}
+                height={150}
+                alt=""
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="mb-3">Lisa Cudrow</h5>
+            <h6 className="text-primary mb-3">Graphic Designer</h6>
+            <p className="px-xl-3">
+              {" "}
+              <i className="fas fa-quote-left pe-2" />
+              <span>
+                Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                corporis suscipit laboriosam, nisi ut aliquid commodi.
+              </span>{" "}
+            </p>
+            <ul className="list-unstyled d-flex justify-content-center mb-0">
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 mb-0">
+            <div className="d-flex justify-content-center mb-4">
+              {" "}
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
+                className="rounded-circle shadow-1-strong"
+                width={150}
+                height={150}
+                alt=""
+                aria-controls="#picker-editor"
+              />{" "}
+            </div>
+            <h5 className="mb-3">John Smith</h5>
+            <h6 className="text-primary mb-3">Marketing Specialist</h6>
+            <p className="px-xl-3">
+              {" "}
+              <i className="fas fa-quote-left pe-2" />
+              <span>
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti.
+              </span>{" "}
+            </p>
+            <ul className="list-unstyled d-flex justify-content-center mb-0">
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-star fa-sm text-warning" />{" "}
+              </li>
+              <li>
+                {" "}
+                <i className="far fa-star fa-sm text-warning" />{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </section>
+    {/**/}
+  </div>
+  <div data-draggable="true" style={{ position: "relative" }}>
+    {/**/}
+    {/**/}
+    <section draggable="false" className="container pt-5" data-v-271253ee="">
+      <section className="mb-10 text-center">
+        <div className="p-5">
+          <h2 className="my-5 display-5 fw-bold ls-tight">
+            {" "}
+            <span>Let's stay in touch</span> <br />{" "}
+            <span className="text-primary">Join our social media</span>{" "}
+          </h2>{" "}
+          {/* Facebook */}{" "}
+          <a
+            className="btn btn-primary btn-lg me-md-2 mb-3"
+            style={{ backgroundColor: "#3b5998" }}
+            href="#!"
+            role="button"
+          >
+            <i
+              className="fab fa-facebook-f me-2"
+              aria-controls="#picker-editor"
+            />{" "}
+            <span>Facebook</span>{" "}
+          </a>
+          {/* Twitter */}{" "}
+          <a
+            className="btn btn-primary btn-lg me-md-2 mb-3"
+            style={{ backgroundColor: "#55acee" }}
+            href="#!"
+            role="button"
+          >
+            {" "}
+            <i className="fab fa-twitter me-2" aria-controls="#picker-editor" />
+            <span>Twitter</span>{" "}
+          </a>{" "}
+          {/* Google */}{" "}
+          <a
+            className="btn btn-primary btn-lg me-md-2 mb-3"
+            style={{ backgroundColor: "#ed302f" }}
+            href="#!"
+            role="button"
+          >
+            {" "}
+            <i
+              className="fab fa-youtube me-2"
+              aria-controls="#picker-editor"
+            />{" "}
+            <span>YouTube</span>{" "}
+          </a>{" "}
+          {/* Instagram */}{" "}
+          <a
+            className="btn btn-primary btn-lg me-md-2 mb-3"
+            style={{ backgroundColor: "#ac2bac" }}
+            href="#!"
+            role="button"
+          >
+            <i
+              className="fab fa-instagram me-2"
+              aria-controls="#picker-editor"
+            />{" "}
+            <span>Instagram</span>{" "}
+          </a>
+        </div>
+      </section>
+    </section>
+    {/**/}
+  </div>
+</div>
 
-        <input type="text" />
-
-        <h2 className='mytag'>React is Awesome</h2>
-
-    </div>
   )
 }
 
