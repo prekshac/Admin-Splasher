196e9db74b3baee29515c211e66b9693eca3e1a9
prekshac <perkschat@gmail.com>
2023-04-27 00:17:36 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/user', userRouter);
+app.use('/users', userRouter);
 
 app.get('/', (req, res)=>{
     res.send('Working perfectly');
---
+++ backend/models/feedbackModel.js
@@ -0,0 +1,10 @@
+const {Schema, model}= require('../connection');
+
+const myschema=new Schema({
+    name: {type: String, required: true},  //mandatory field
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    createdAt: Date
+});
+
+module.exports=model('feedback', myschema);
