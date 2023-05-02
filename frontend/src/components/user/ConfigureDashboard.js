65a95c47e7b9798abd2082fb5f472592c665cb2f
ArihantSri <arihantsrivastava070@gmail.com>
2023-05-02 16:18:59 +0530

    .......

--- frontend/src/components/user/ConfigureDashboard.js
+++ frontend/src/components/user/ConfigureDashboard.js
@@ -1,30 +1,47 @@
 import React, { useEffect } from 'react';
 import db from '../../firebaseConfig';
+import { useState } from 'react';
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore"
 
 
 const ConfigureDashboard = () => {
 
   const [data, setData] = useState([]);
-  
-    // const getFirebaseData = () => {
-    //   const collection = db.collection("product")
-    //   .onSnapshot((querySnapshot) => {
-    //     const newData = [];
-    //     querySnapshot.forEach((doc) => {
-    //       newData.push(doc.data());
-    //     });
-    //     setData(newData);
-    //   });
-    // };
 
-    useEffect(() => {
-      // getFirebaseData();
-    }, []);
-    
+  const getFirebaseData = () => {
+    const q = query(collection(db, 'product'))
+    onSnapshot(q, (querySnapshot) => {
+      console.log(querySnapshot.docs);
+      setData(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+  };
+
+  const displayDocs = () => {
+    return data.map(({data, id}) => {
+      return (
+        <div key={id}>
+          <h1>{data.name}</h1>
+          <h1>{data.price}</h1>
+          <h1>{data.category}</h1>
+          <h1>{data.brand}</h1>
+        </div>
+      )
+    })
+  }
+
+  useEffect(() => {
+    getFirebaseData();
+  }, []);
+
 
   return (
-    <div>ConfigureDashboard</div>
+    <div>
+      {displayDocs()}
+    </div>
   )
 }
 
-export default ConfigureDashboard;
+export default ConfigureDashboard
