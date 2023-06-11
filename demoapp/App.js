e35acbcaa28a597c6952f30eac7e6c75a0e5bf37
m-triple-m <triplem656@gmail.com>
2023-06-11 20:14:01 +0530

    ..

--- demoapp/App.js
+++ demoapp/App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontIcon from 'react-native-vector-icons/FontAwesome';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import ListMenu from './screens/ListMenu';
+import Menu from './screens/Menu';
+import CurrentOrder from './screens/CurrentOrder';
+import { useState } from 'react';
+
+
 
 export default function App() {
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCpjPJCT-bO49GkuQq982fVp3Ju0sm5prM",
+    authDomain: "native-9ba01.firebaseapp.com",
+    projectId: "native-9ba01",
+    storageBucket: "native-9ba01.appspot.com",
+    messagingSenderId: "140174126073",
+    appId: "1:140174126073:web:f79e36a08b365da9572b28",
+    measurementId: "G-6W3KPL713D",
+  };
+  const app = initializeApp(firebaseConfig);
+  const Tab = createBottomTabNavigator();
+
+  
+  
+
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight: 'bold', fontSize: 50}}>Admin Splasher Demo App!</Text>
-      <StatusBar style="auto" />
+      
+      <NavigationContainer style={styles.container} >
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            size = focused ? 25 : 20;
+            if (route.name === "Profile") {
+              iconName = focused ? "user-circle" : "user-circle-o";
+            } else if (route.name === "ListMenu") {
+              iconName = focused ? "list-alt" : "list-alt";
+            } else if (route.name === "Home") {
+              iconName = focused ? "address-book" : "address-book-o";
+            }else if (route.name === "ListPost") {
+              iconName = focused ? "post" : "post-outline";
+            }
+    
+            // You can return any component that you like here!
+            return <FontIcon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" children={()=><Home app={app}/>} options={{
+          headerShown: false,
+        }} />
+        <Tab.Screen name="ListMenu" children={()=><ListMenu app={app}/>} options={{
+          headerShown: false,
+        }} />
+        {/* <Tab.Screen name="Profile" component={Profile} /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
